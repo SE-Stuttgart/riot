cd9152e76cfd2323f3d2ad2f28f107607e3ac4c4
Rocco Schulz <schocco.rulz@gmail.com>
2014-12-07 15:29:53 +0100

    Add ConnectionManager class to get connections from the connection pool. Add description for DB setup and config of connection pool resource.

--- webapp/README.md
+++ webapp/README.md
@@ -47,3 +47,19 @@ Install the following plugins from the eclipse marketplace if not installed alre
   - Check that the imported formatter is selected in the project specific settings
 - Windows -> Preferences -> Java -> Editor -> Save Actions -> Perform all selected actions on save
 - Right-click on Project -> Checkstyle -> Activate Checkstyle 
+
+## Database Setup
+
+- install the latest mysql-server
+- open the mySQL shell as administrative user (e.g. with `mysql -u root -p`)
+  - create a user for the riot application: `CREATE USER 'riot'@'localhost' IDENTIFIED BY 'password';`
+  - create a database for the riot app: `CREATE DATABASE riot CHARACTER SET utf8;`
+  - grant permissions: `GRANT ALL ON riot.* TO 'riot'@'%' IDENTIFIED BY 'riot1234' WITH GRANT OPTION; FLUSH PRIVILEGES;`
+- download the mysql-connector jar from https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.34.zip
+- unzip the package and put the jar into the domain/lib folder of your glassfish installation
+- restart glassfish and open the admin panel: http://localhost:4848/common/index.jsf
+- create a new JDBC connection pool to mysql JDBC->JDBC Connection Pools->New...
+  - Select MySql as Database Driver Vendor and javax.sql.ConnectionPoolDataSource as Resource Type, click next
+  - add properties "user:riot", "password:yourpassword", "databaseName:riot" and save
+- go back to jdbc and create a resource with the jndi name "jdbc/riot-pool" and reference the connection pool
+- the jndi name can now be used to access the pool in the jee containers
---
+++ webapp/src/main/java/de/uni_stuttgart/riot/db/ConnectionMgr.java
@@ -0,0 +1,63 @@
+package de.uni_stuttgart.riot.db;
+
+import java.sql.SQLException;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+/**
+ * A util class for quick access to connection instances.
+ */
+public abstract class ConnectionMgr {
+
+    /**
+     * JNDI name of the jdbc resource as configured on the jee server.
+     */
+    private static final String DATASOURCE_NAME = "jdbc/riot-pool";
+
+    /**
+     * Gets the DataSource. Could probably be injected with CDI.
+     * 
+     * @return DataSource
+     * @throws NamingException
+     *             if the resource is not configured in the jee server.
+     */
+    private static DataSource getDataSource() throws NamingException {
+        InitialContext ctx = new InitialContext();
+        DataSource ds = (DataSource) ctx.lookup(DATASOURCE_NAME);
+        return ds;
+    }
+
+    /**
+     * Creates a sql2o instance. Use {@link #openConnection()} in a 
+     * try-with-resources block if you do not need to perform transactions.
+     * 
+     * @return an sql2o instance with an assigned DataSource
+     * @throws NamingException if the resource is not configured in the jee server.
+     * @throws SQLException if a DB error occurs
+     */
+    public static Sql2o getSql2o() throws NamingException, SQLException {
+        DataSource ds = getDataSource();
+        Sql2o sql2o = new Sql2o(ds);
+        return sql2o;
+    }
+
+    /**
+     * Opens an sql2o connection and returns it.
+     * Use {@link #getSql2o()} if transactions are needed.
+     * 
+     * @return An open sql2o connection
+     * @throws NamingException if the resource is not configured in the jee server.
+     * @throws SQLException if a DB error occurs
+     */
+    public static Connection openConnection() throws NamingException, SQLException {
+        DataSource ds = getDataSource();
+        Sql2o sql2o = new Sql2o(ds);
+        return sql2o.open();
+    }
+
+}
