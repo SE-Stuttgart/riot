eb393f9c75e98eb1b5aa175950e0b6d424f631ee
Philipp Keck <git@philippkeck.de>
2014-12-13 21:48:38 +0100

    RIOT-59: UserManagement Code aus project-Ordner raus verschoben

---
+++ usermanagement/src/main/java/de/uni_stuttgart/riot/usermanagement/service/response/AuthenticationResponse.java
@@ -0,0 +1,49 @@
+package de.uni_stuttgart.riot.usermanagement.service.response;
+
+import de.uni_stuttgart.riot.usermanagement.service.AuthenticationService;
+
+/**
+ * The authentication response message for the client used in {@link AuthenticationService}.
+ * 
+ * @author Marcel Lehwald
+ *
+ */
+public class AuthenticationResponse {
+
+    private String accessToken;
+    private String refreshToken;
+
+    public AuthenticationResponse() {
+
+    }
+
+    /**
+     * Create a authentication response.
+     * 
+     * @param accessToken
+     *            The valid access token generated when the user was successfully authenticated.
+     * @param refreshToken
+     *            The valid refresh token generated when the user was successfully authenticated.
+     */
+    public AuthenticationResponse(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+}
