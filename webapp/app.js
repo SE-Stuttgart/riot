bb3ee306ebd8cd234893597bbac3e2d00ce79417
Marcel Lehwald <marcel.lehwald@googlemail.com>
2015-01-24 21:42:38 +0100

    webapp: added error message when login fails

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,6 @@
 angular.module('riot', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'restangular', 'LocalStorageModule']);
 
-angular.module('riot').run(function($rootScope, $state, Auth) {
+angular.module('riot').run(function($rootScope, $state, $sce, Auth) {
   //check authorization on state change
   $rootScope.$on('$stateChangeStart', function(e, state) {
     state.data = state.data || {};
@@ -33,5 +33,33 @@ angular.module('riot').run(function($rootScope, $state, Auth) {
         $state.go('landing.home');
       }
     }
+  });
+
+  //dialog
+  $rootScope.dialog = {
+    header: '',
+    body: '',
+    footer: '<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>',
+    show: function() {
+      $('#dialog').modal('show');
+    },
+    hide: function() {
+      $('#dialog').modal('hide');
+    }
+  };
+
+  $rootScope.$watch('dialog.header', function(newValue, oldValue) {
+    if (typeof newValue == 'string')
+      $rootScope.dialog.header = $sce.trustAsHtml(newValue || '');
+  });
+
+  $rootScope.$watch('dialog.body', function(newValue, oldValue) {
+    if (typeof newValue == 'string')
+      $rootScope.dialog.body = $sce.trustAsHtml(newValue || '');
+  });
+
+  $rootScope.$watch('dialog.footer', function(newValue, oldValue) {
+    if (typeof newValue == 'string')
+      $rootScope.dialog.footer = $sce.trustAsHtml(newValue || '');
   });
 });
