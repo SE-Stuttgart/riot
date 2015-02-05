1e9f8d9de2c64cd321ab4a6eea17b92b9bb3d716
Niklas Schnabel <st112134@stud.uni-stuttgart.de>
2015-02-06 00:15:37 +0100

    Fixed webapp test, so that the build works again

--- webapp/commons/resources/configuration/configuration-spec.js
+++ webapp/commons/resources/configuration/configuration-spec.js
@@ -2,7 +2,7 @@ describe('configuration', function() {
 
   beforeEach(module('riot'));
 
-  it('should ...', inject(function(configuration) {
+  it('should ...', inject(function(Configuration) {
 
 	//expect(configuration.doSomething()).toEqual('something');
 
