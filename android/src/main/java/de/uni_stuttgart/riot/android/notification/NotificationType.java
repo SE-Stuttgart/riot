51cdd38bf9c3f25ffec5616f30300a3dbc69e0d5
christoph.ebbers <christoph.ebbers@gmail.com>
2015-02-06 12:07:35 +0100

    RIOT-155 refactoring notifications

---
+++ android/src/main/java/de/uni_stuttgart/riot/android/notification/NotificationType.java
@@ -0,0 +1,6 @@
+package de.uni_stuttgart.riot.android.notification;
+
+//CHECKSTYLE:OFF FIXME Please fix the checkstyle errors in this file and remove this comment.
+public enum NotificationType {
+    VOID, ERROR, WARNING, APPOINTMENT;
+}
