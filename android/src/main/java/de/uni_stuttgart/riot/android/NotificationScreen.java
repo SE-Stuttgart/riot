a959d6275ee0455e71a833484bace1fbad96bee6
Christoph Ebbers <christoph.ebbers@gmail.com>
2015-02-08 23:21:33 +0100

    RIOT-155 added SSL zertificate, keystore and HttpClient

--- android/src/main/java/de/uni_stuttgart/riot/android/NotificationScreen.java
+++ android/src/main/java/de/uni_stuttgart/riot/android/NotificationScreen.java
@@ -25,109 +25,121 @@ import de.uni_stuttgart.riot.android.notification.NotificationType;
  */
 public class NotificationScreen extends Activity {
 
-    private ListView notificationList;
-    private RIOTDatabase database;
-    private String pressedHomeScreenButton;
+	private ListView notificationList;
+	private RIOTDatabase database;
+	private String pressedHomeScreenButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Intent intent = getIntent();
 
-        // get the value of the pressed button
-        pressedHomeScreenButton = intent.getStringExtra("pressedButton");
+		// get the value of the pressed button
+		pressedHomeScreenButton = intent.getStringExtra("pressedButton");
 
-        database = new RIOTDatabase(this, pressedHomeScreenButton);
+		database = new RIOTDatabase(this, pressedHomeScreenButton);
 
-        // Sets the language
-        Language.setLanguage(this);
+		// Sets the language
+		Language.setLanguage(this);
 
-        setContentView(R.layout.notification_screen);
+		setContentView(R.layout.notification_screen);
 
-        getActionBar().setHomeButtonEnabled(true);
-        getActionBar().setTitle(pressedHomeScreenButton.substring(0, 1).toUpperCase() + pressedHomeScreenButton.substring(1));
+		getActionBar().setHomeButtonEnabled(true);
+		getActionBar().setTitle(
+				pressedHomeScreenButton.substring(0, 1).toUpperCase()
+						+ pressedHomeScreenButton.substring(1));
 
-        if (pressedHomeScreenButton.equals("car")) {
-            getActionBar().setIcon(R.drawable.car);
-        } else if (pressedHomeScreenButton.equals("house")) {
-            getActionBar().setIcon(R.drawable.house);
-        } else if (pressedHomeScreenButton.equals("coffeeMachine")) {
-            getActionBar().setIcon(R.drawable.coffee);
-        }
+		if (pressedHomeScreenButton.equals("car")) {
+			getActionBar().setIcon(R.drawable.car);
+		} else if (pressedHomeScreenButton.equals("house")) {
+			getActionBar().setIcon(R.drawable.house);
+		} else if (pressedHomeScreenButton.equals("coffeeMachine")) {
+			getActionBar().setIcon(R.drawable.coffee);
+		}
 
-        // ClickListener for the Notification List
-        notificationList = (ListView) findViewById(R.id.NotificationList);
-        notificationList.setOnItemClickListener(new OnItemClickListener() {
+		// ClickListener for the Notification List
+		notificationList = (ListView) findViewById(R.id.NotificationList);
+		notificationList.setOnItemClickListener(new OnItemClickListener() {
 
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                System.out.println("click");
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				System.out.println("click");
 
-            }
-        });
+			}
+		});
 
-        if (isNetworkAvailable()) {
-            // get the latest Notifications
-            new ServerConnection(this, database).execute();
-        } else {
-            Toast.makeText(this, "No internet connection", Toast.LENGTH_SHORT).show();
-        }
-    }
+		if (isNetworkAvailable()) {
+			// get the latest Notifications
+			new ServerConnection(this, database).execute();
+		} else {
+			Toast.makeText(this, "No internet connection", Toast.LENGTH_LONG)
+					.show();
+		}
+	}
 
-    private boolean isNetworkAvailable() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-    }
+	private boolean isNetworkAvailable() {
+		ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo activeNetworkInfo = connectivityManager
+				.getActiveNetworkInfo();
+		return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+	}
 
-    /**
-     * Prepare the refresh button on the right side
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+	/**
+	 * Prepare the refresh button on the right side
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    /**
-     * Define displaying settings for the refresh button
-     */
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
+	/**
+	 * Define displaying settings for the refresh button
+	 */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
 
-        // //get the values for the each filter from the database
-        menu.findItem(R.id.filter_error).setChecked(database.getFilterSettings(NotificationType.ERROR));
-        menu.findItem(R.id.filter_warning).setChecked(database.getFilterSettings(NotificationType.WARNING));
-        menu.findItem(R.id.filter_appointment).setChecked(database.getFilterSettings(NotificationType.APPOINTMENT));
+		// //get the values for the each filter from the database
+		menu.findItem(R.id.filter_error).setChecked(
+				database.getFilterSettings(NotificationType.ERROR));
+		menu.findItem(R.id.filter_warning).setChecked(
+				database.getFilterSettings(NotificationType.WARNING));
+		menu.findItem(R.id.filter_appointment).setChecked(
+				database.getFilterSettings(NotificationType.APPOINTMENT));
 
-        menu.findItem(R.id.action_refresh).setVisible(true);
-        database.filterNotifications();
+		menu.findItem(R.id.action_refresh).setVisible(true);
+		database.filterNotifications();
 
-        return super.onPrepareOptionsMenu(menu);
-    }
+		return super.onPrepareOptionsMenu(menu);
+	}
 
-    /**
-     * Actions for the refresh button (right upper corner). It can later be extended with more options.
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+	/**
+	 * Actions for the refresh button (right upper corner). It can later be
+	 * extended with more options.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch (item.getItemId()) {
-        case R.id.filter_error:
-            database.setFilter(new FilterItem(1, item, NotificationType.ERROR, false));
-            return true;
-        case R.id.filter_appointment:
-            database.setFilter(new FilterItem(2, item, NotificationType.APPOINTMENT, false));
-            return true;
-        case R.id.filter_warning:
-            database.setFilter(new FilterItem(3, item, NotificationType.WARNING, false));
-            return true;
-        case R.id.action_refresh:
-            new ServerConnection(this, database).execute();
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-    }
+		switch (item.getItemId()) {
+		case R.id.filter_error:
+			database.setFilter(new FilterItem(1, item, NotificationType.ERROR,
+					false));
+			return true;
+		case R.id.filter_appointment:
+			database.setFilter(new FilterItem(2, item,
+					NotificationType.APPOINTMENT, false));
+			return true;
+		case R.id.filter_warning:
+			database.setFilter(new FilterItem(3, item,
+					NotificationType.WARNING, false));
+			return true;
+		case R.id.action_refresh:
+			new ServerConnection(this, database).execute();
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 
 }
--- android/src/main/java/de/uni_stuttgart/riot/android/communication/RIOTApiClient.java
+++ android/src/main/java/de/uni_stuttgart/riot/android/communication/RIOTApiClient.java
@@ -2,110 +2,123 @@ package de.uni_stuttgart.riot.android.communication;
 
 import java.io.IOException;
 
-import android.content.Context;
 import android.os.Looper;
 import android.os.NetworkOnMainThreadException;
+import de.uni_stuttgart.riot.android.HomeScreen;
 import de.uni_stuttgart.riot.clientlibrary.usermanagement.client.LoginClient;
 import de.uni_stuttgart.riot.clientlibrary.usermanagement.client.UsermanagementClient;
 
 /**
- * Client for communicating with the RIOT API. It is not allowed to use this client in the main thread.
+ * Client for communicating with the RIOT API. It is not allowed to use this
+ * client in the main thread.
  * 
  * @author Niklas Schnabel
  */
 public class RIOTApiClient {
 
-    /** The Constant API_URL. */
-    private static final String API_URL = "https://belgrad.informatik.uni-stuttgart.de:8181/riot/"; // TODO url, 10.0.2.2 is localhost for the android emulator
+	/** The Constant API_URL. */
+	private static final String API_URL = "https://belgrad.informatik.uni-stuttgart.de:8181"; // TODO
+																								// url,
+																								// 10.0.2.2
+																								// is
+																								// localhost
+																								// for
+																								// the
+																								// android
+																								// emulator
 
-    /** The instance. */
-    private static RIOTApiClient instance;
+	/** The instance. */
+	private static RIOTApiClient instance;
 
-    /** The init. */
-    private boolean init;
+	/** The init. */
+	private boolean init;
 
-    /** The login client. */
-    private LoginClient loginClient;
+	/** The login client. */
+	private LoginClient loginClient;
 
-    /** The user management client. */
-    private UsermanagementClient userManagementClient;
+	/** The user management client. */
+	private UsermanagementClient userManagementClient;
 
-    /**
-     * Instantiates a new RIOT api client.
-     */
-    private RIOTApiClient() {
-    }
+	/**
+	 * Instantiates a new RIOT api client.
+	 */
+	private RIOTApiClient() {
+	}
 
-    /**
-     * Gets the single instance of RIOTApiClient. It is <b>NOT</b> allowed to use the RIOTApiClient in the main thread!
-     *
-     * @return single instance of RIOTApiClient
-     */
-    public static RIOTApiClient getInstance() {
-        if (instance == null) {
-            instance = new RIOTApiClient();
-        }
-        return instance;
-    }
+	/**
+	 * Gets the single instance of RIOTApiClient. It is <b>NOT</b> allowed to
+	 * use the RIOTApiClient in the main thread!
+	 *
+	 * @return single instance of RIOTApiClient
+	 */
+	public static RIOTApiClient getInstance() {
+		if (instance == null) {
+			instance = new RIOTApiClient();
+		}
+		return instance;
+	}
 
-    /**
-     * Initializes the API Client. Has to be called before first usage or a Runtime Exception will be thrown.
-     *
-     * @param context
-     *            the activity
-     * @param deviceName
-     *            the device name
-     */
-    public void init(Context context, String deviceName) {
-        throwExceptionIfOnMainThread();
+	/**
+	 * Initializes the API Client. Has to be called before first usage or a
+	 * Runtime Exception will be thrown.
+	 *
+	 * @param context
+	 *            the activity
+	 * @param deviceName
+	 *            the device name
+	 */
+	public void init(HomeScreen context, String deviceName) {
+		throwExceptionIfOnMainThread();
 
-        try {
-            loginClient = new LoginClient(API_URL, deviceName, new TokenManager(context));
-        } catch (IOException e) {
-            // FIXME use NotificationManagers
-        }
-        userManagementClient = new UsermanagementClient(loginClient);
+		try {
+			loginClient = new LoginClient(API_URL, deviceName,
+					new TokenManager(context), new MyHttpClient(context));
+		} catch (IOException e) {
+			// FIXME use NotificationManagers
+		}
+		userManagementClient = new UsermanagementClient(loginClient);
 
-        init = true;
-    }
+		init = true;
+	}
 
-    /**
-     * Gets the login client.
-     *
-     * @return the login client
-     */
-    public LoginClient getLoginClient() {
-        throwExceptionIfOnMainThread();
-        checkIfInit();
-        return loginClient;
-    }
+	/**
+	 * Gets the login client.
+	 *
+	 * @return the login client
+	 */
+	public LoginClient getLoginClient() {
+		throwExceptionIfOnMainThread();
+		checkIfInit();
+		return loginClient;
+	}
 
-    /**
-     * Gets the user management client.
-     *
-     * @return the user management client
-     */
-    public UsermanagementClient getUserManagementClient() {
-        throwExceptionIfOnMainThread();
-        checkIfInit();
-        return userManagementClient;
-    }
+	/**
+	 * Gets the user management client.
+	 *
+	 * @return the user management client
+	 */
+	public UsermanagementClient getUserManagementClient() {
+		throwExceptionIfOnMainThread();
+		checkIfInit();
+		return userManagementClient;
+	}
 
-    /**
-     * Check if init.
-     */
-    private void checkIfInit() {
-        if (!init) {
-            throw new RuntimeException("RiotApiClient has to be initialized before first usage. Call init() first!");
-        }
-    }
+	/**
+	 * Check if init.
+	 */
+	private void checkIfInit() {
+		if (!init) {
+			throw new RuntimeException(
+					"RiotApiClient has to be initialized before first usage. Call init() first!");
+		}
+	}
 
-    /**
-     * Throw exception if the current thread is the main thread.
-     */
-    private void throwExceptionIfOnMainThread() {
-        if (Looper.myLooper() == Looper.getMainLooper()) {
-            throw new NetworkOnMainThreadException();
-        }
-    }
+	/**
+	 * Throw exception if the current thread is the main thread.
+	 */
+	private void throwExceptionIfOnMainThread() {
+		if (Looper.myLooper() == Looper.getMainLooper()) {
+			throw new NetworkOnMainThreadException();
+		}
+	}
 }
