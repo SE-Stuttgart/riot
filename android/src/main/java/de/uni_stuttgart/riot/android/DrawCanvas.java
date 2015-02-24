01761743a1a79585353fd0ae2477c14a8621da71
Florian <florian.gaensslen@googlemail.com>
2015-02-24 18:51:59 +0100

    Extended Canvas

--- android/src/main/java/de/uni_stuttgart/riot/android/DrawCanvas.java
+++ android/src/main/java/de/uni_stuttgart/riot/android/DrawCanvas.java
@@ -1,6 +1,7 @@
 package de.uni_stuttgart.riot.android;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -14,34 +15,46 @@ import de.enpro.android.riot.R;
 //http://www.milestone-blog.de/android-development/einfaches-zeichnen-canvas/
 
 public class DrawCanvas extends View implements OnTouchListener {
-
-    private Canvas canvas;
+    Bitmap bitmap;
+    Canvas bitmapCanvas;
+    boolean isInitialized;
     Paint paint = new Paint();
 
     public DrawCanvas(Context context) {
         super(context);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
         this.setOnTouchListener(this);
-
         paint.setColor(Color.RED);
         paint.setAntiAlias(true);
         paint.setStyle(Style.FILL_AND_STROKE);
 
-        canvas = new Canvas();
+        isInitialized = false;
+    }
+
+    private void init() {
+        bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.RGB_565);
+
+        bitmapCanvas = new Canvas();
+        bitmapCanvas.setBitmap(bitmap);
+        bitmapCanvas.drawColor(Color.BLACK);
+
+        isInitialized = true;
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
-        // canvas.drawColor(Color.RED);
+    public void onDraw(Canvas canvas) {
+        if (!isInitialized)
+            init();
+        canvas.drawBitmap(bitmap, 0, 0, paint);
         canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.car, new BitmapFactory.Options()), 100, 100, null);
     }
 
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        canvas.drawColor(Color.RED);
-        canvas.drawColor(Color.GREEN);
-        // canvas.drawCircle(event.getX(), event.getY(), 500, paint);
-        System.out.println(event.getX());
-        // invalidate();
+    public boolean onTouch(View view, MotionEvent event) {
+        bitmapCanvas.drawCircle(event.getX(), event.getY(), 5, paint);
+
+        invalidate();
         return true;
     }
+
 }
