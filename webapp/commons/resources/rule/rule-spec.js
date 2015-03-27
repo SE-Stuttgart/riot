5463043bec6e5f74450994748b7c2590a9421a1c
Niklas Schnabel <st112134@stud.uni-stuttgart.de>
2015-03-27 13:32:38 +0100

    RIOT-299 Fix of broken test

--- webapp/commons/resources/rule/rule-spec.js
+++ webapp/commons/resources/rule/rule-spec.js
@@ -1,8 +1,8 @@
-describe('rule', function() {
+describe('Rule', function() {
 
   beforeEach(module('riot'));
 
-  it('should ...', inject(function(rule) {
+  it('should ...', inject(function(Rule) {
 
 	//expect(rule.doSomething()).toEqual('something');
 
